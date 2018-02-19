0a980928152c78b43422df54fa05439dbb2fd32b
Dmitry Stupakov <dstupakov@gmail.com>
2018-02-18 21:22:17 -0600

    Trying to show some health data.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,9 @@ export class HomePage {
   totalFat;
   totalProtein;
   totalCarbs;
+  weight;
  
-  constructor(private platform: Platform, public navCtrl: NavController, private health: Health) {
+  constructor(private platform: Platform, public navCtrl: NavController, private health: Health, private firebase: Firebase) {
     this.platform.ready().then(() => {
       this.health.isAvailable()
       .then((available:boolean) => {
@@ -23,15 +24,26 @@ export class HomePage {
           this.getFat()
           this.getProtein()
           this.getCarbs()
+          this.getWeight()
         })
         .catch(e => console.log(e));
       })
       .catch(e => console.log(e)); 
   }
-
+  getWeight() {
+    this.health.query({
+      startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
+      endDate: new Date(), // now
+      dataType: 'weight', 
+    }).then(weight => {
+      this.weight = weight
+      console.log(this.weight)
+    })
+  }
+  
   getFat() {
     this.health.queryAggregated({
-      startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // three days ago
+      startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
       endDate: new Date(), // now
       dataType: 'nutrition.fat.total', 
       bucket: 'day'
@@ -43,7 +55,7 @@ export class HomePage {
 
   getProtein() {
     this.health.queryAggregated({
-      startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // three days ago
+      startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
       endDate: new Date(), // now
       dataType: 'nutrition.protein', 
       bucket: 'day'
@@ -56,7 +68,7 @@ export class HomePage {
   getCarbs() {
     // Get Carbs
     this.health.queryAggregated({
-      startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // three days ago
+      startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
       endDate: new Date(), // now
       dataType: 'nutrition.carbs.total', 
       bucket: 'day'
