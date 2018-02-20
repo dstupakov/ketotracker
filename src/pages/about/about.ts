61d1952727acf204eeb6f825554f9c5e7716b8fd
Dmitry Stupakov <dstupakov@gmail.com>
2018-02-19 19:48:37 -0600

    Hardcoded some test values to explore in Chrome. about.ts gets blood glucose values now.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Health } from '@ionic-native/health';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  constructor(public navCtrl: NavController) {
+  nutritionFat;
+  bloodGlucose;
+
+  constructor(public navCtrl: NavController, private health: Health) {
+    this.health.isAvailable()
+    .then((available:boolean) => {
+      this.health.requestAuthorization([{
+          read: ['steps', 'height', 'weight', 'nutrition'],       //read only permission
+        }])
+      })
+      .then(_ => {
+        this.getNutritionFat()
+        this.getBloodGlucose()
+      })
+      .catch(e => console.log(e));
+    }
+    getNutritionFat () {
+      this.health.query({
+        startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
+        endDate: new Date(), // now
+        dataType: 'nutrition.total.fat', 
+      }).then(data => {
+        this.nutritionFat = data
+        console.log(this.nutritionFat)
+      })
+    } 
+    getBloodGlucose () {
+      this.health.query({
+        startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
+        endDate: new Date(), // now
+        dataType: 'blood_glucose', 
+      }).then(data => {
+        this.bloodGlucose = data
+        console.log(this.bloodGlucose)
+      })
+    } 
   }
-}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,11 +7,13 @@ import { NavController, Platform } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  totalFat;
-  totalProtein;
-  totalCarbs;
+  totalFat = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":124.29999999999998,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":108.27,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":148.595,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":100.1,"unit":"g"}];
+  totalProtein = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":80.25,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":111.77000000000001,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":123.20166666666668,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":121.27666666666667,"unit":"g"}];
+  totalCarbs = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":46,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":40.09999990463257,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":64.76000022888184,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":50,"unit":"g"}];
   weight;
- 
+
+  summary = Object.assign({}, this.totalFat, this.totalProtein, this.totalCarbs);
+
   constructor(private platform: Platform, public navCtrl: NavController, private health: Health) {
     this.platform.ready().then(() => {
       this.health.isAvailable()
@@ -48,7 +50,7 @@ export class HomePage {
       dataType: 'nutrition.fat.total', 
       bucket: 'day'
     }).then(fatData => {
-      this.totalFat = fatData
+      // this.totalFat = fatData
       console.log(this.totalFat)
     })
   }
@@ -60,7 +62,7 @@ export class HomePage {
       dataType: 'nutrition.protein', 
       bucket: 'day'
     }).then(proteinData => {
-      this.totalProtein = proteinData
+      // this.totalProtein = proteinData
       console.log(this.totalProtein)
     })
   }
@@ -73,7 +75,7 @@ export class HomePage {
       dataType: 'nutrition.carbs.total', 
       bucket: 'day'
     }).then(carbData => {
-      this.totalCarbs = carbData
+      // this.totalCarbs = carbData
       console.log(this.totalCarbs)
     })
   }
