d0f8a1a797b0d5b1c6319d46a416c24c5b7c068c
Dmitry Stupakov <dstupakov@gmail.com>
2018-02-24 14:30:57 -0600

    Added combine function, not sure why it's triggering too early.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,12 +7,11 @@ import { NavController, Platform } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  totalFat = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":124.29999999999998,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":108.27,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":148.595,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":100.1,"unit":"g"}];
-  totalProtein = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":80.25,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":111.77000000000001,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":123.20166666666668,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":121.27666666666667,"unit":"g"}];
-  totalCarbs = [{"startDate":"2018-02-11T06:00:00.000Z","endDate":"2018-02-12T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-12T06:00:00.000Z","endDate":"2018-02-13T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-13T06:00:00.000Z","endDate":"2018-02-14T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-14T06:00:00.000Z","endDate":"2018-02-15T06:00:00.000Z","value":0,"unit":"g"},{"startDate":"2018-02-15T06:00:00.000Z","endDate":"2018-02-16T06:00:00.000Z","value":46,"unit":"g"},{"startDate":"2018-02-16T06:00:00.000Z","endDate":"2018-02-17T06:00:00.000Z","value":40.09999990463257,"unit":"g"},{"startDate":"2018-02-17T06:00:00.000Z","endDate":"2018-02-18T06:00:00.000Z","value":64.76000022888184,"unit":"g"},{"startDate":"2018-02-18T06:00:00.000Z","endDate":"2018-02-19T06:00:00.000Z","value":50,"unit":"g"}];
+  totalFat= [];
+  totalProtein = [];
+  totalCarbs = [];
   weight;
-
-  summary = Object.assign({}, this.totalFat, this.totalProtein, this.totalCarbs);
+  combinedData = [];
 
   constructor(private platform: Platform, public navCtrl: NavController, private health: Health) {
     this.platform.ready().then(() => {
@@ -28,6 +27,10 @@ export class HomePage {
           this.getCarbs()
           this.getWeight()
         })
+        .then(_ => {
+          this.combinedData = this.combineData(this.totalFat, this.totalProtein, this.totalCarbs)
+          console.log("Combined Data:" + this.combinedData)
+        })
         .catch(e => console.log(e));
       })
       .catch(e => console.log(e)); 
@@ -36,10 +39,10 @@ export class HomePage {
     this.health.query({
       startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // three days ago
       endDate: new Date(), // now
-      dataType: 'weight', 
+      dataType: 'weight'
     }).then(weight => {
       this.weight = weight
-      console.log(this.weight)
+      console.log("Weight:" + this.weight)
     })
   }
   
@@ -50,8 +53,8 @@ export class HomePage {
       dataType: 'nutrition.fat.total', 
       bucket: 'day'
     }).then(fatData => {
-      // this.totalFat = fatData
-      console.log(this.totalFat)
+      this.totalFat = fatData
+      console.log("Fat:" + this.totalFat)
     })
   }
 
@@ -62,8 +65,8 @@ export class HomePage {
       dataType: 'nutrition.protein', 
       bucket: 'day'
     }).then(proteinData => {
-      // this.totalProtein = proteinData
-      console.log(this.totalProtein)
+      this.totalProtein = proteinData
+      console.log("Protein:" + this.totalProtein)
     })
   }
 
@@ -75,8 +78,40 @@ export class HomePage {
       dataType: 'nutrition.carbs.total', 
       bucket: 'day'
     }).then(carbData => {
-      // this.totalCarbs = carbData
-      console.log(this.totalCarbs)
+      this.totalCarbs = carbData
+      console.log("Carbs:" + this.totalCarbs)
+    })
+  }
+
+  combineData = (totalFat, totalProtein, totalCarbs) => {
+    // define getStartDates to be a helper function that you can call on a list (such as totalFat) and it returns a list of the start dates that correspond to the elements in the list.
+    const getStartDates = (list) => list.map(v => v.startDate)
+
+    // get a list of start dates from one list (totalFat). 
+    // NOTE: Assuming all lists have the same start dates.
+    const totalFatStartDates = getStartDates(totalFat);
+
+    // map from these start dates to a set of new complete objects that include all data for each start date (compiled from all 3 lists) and return the resulting array of objects.
+    return totalFatStartDates.map(startDate => {
+      var totalCarbsValue = totalCarbs.filter(tc => tc.startDate == startDate)[0]
+      var totalProteinValue = totalProtein.filter(tp => tp.startDate == startDate)[0]
+      var totalFatValue = totalFat.filter(tf => tf.startDate == startDate)[0]
+
+      return {
+        "startDate":startDate,
+        "endDate":totalCarbsValue.endDate,
+        "data": {
+          "totalCarbs": {
+            "value":totalCarbsValue.value,"unit":totalCarbsValue.unit
+          },
+          "totalProtein": {
+            "value":totalProteinValue.value,"unit":totalProteinValue.unit
+          },
+          "totalFat": {
+            "value":totalFatValue.value,"unit":totalFatValue.unit
+          }
+        }
+      }
     })
   }
 }
